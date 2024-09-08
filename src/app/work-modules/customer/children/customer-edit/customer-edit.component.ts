f193c0a2646c7ca6cc39f9b12daa2571bec7d378
Rodolfo <rodolfo.singular@gmail.com>
2024-09-08 19:07:26 -0300

    Create: creando los componentes para editar un cliente

--- src/app/work-modules/customer/children/customer-edit/customer-edit.component.ts
+++ src/app/work-modules/customer/children/customer-edit/customer-edit.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-customer-edit',
-  templateUrl: './customer-edit.component.html',
-  styleUrl: './customer-edit.component.scss'
+  templateUrl: './customer-edit.component.html'
 })
 export class CustomerEditComponent implements OnInit {
   ngOnInit(): void {
