b23ade32be01285fc513f3cb8f821c7f36f5762c
Rodolfo <rodolfo.singular@gmail.com>
2024-10-16 16:09:58 -0300

    Fix: trabajando en la vista del event

--- src/app/layouts/event/event.component.ts
+++ src/app/layouts/event/event.component.ts
@@ -1,9 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MaterialModule } from 'src/app/material/material/material.module';
 
 @Component({
   selector: 'app-event',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MaterialModule
+  ],
   templateUrl: './event.component.html',
   styleUrl: './event.component.scss'
 })
